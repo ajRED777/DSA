ff20b99e358297bfa8c0a1a3ecb688fdaacf42aa
Alan J <44117428+ajRED777@users.noreply.github.com>
2022-06-29 10:21:13 +0000

    Reverse String:Added recursion method

--- DSA/Recursion/Reverse/ReverseString.java
+++ DSA/Recursion/Reverse/ReverseString.java
@@ -9,38 +9,52 @@ class ReverseString {
     System.out.println("You have entered: "+stringToBeReversed);
 
     //Reversing String thru Brute force approach
-    long startTime=0;
-    long endTime=0;
+    float startTime=0;
+    float endTime=0;
+    
     startTime = System.nanoTime();
-    System.out.println("\nString reverse thru brute force method is : ");
+    System.out.print("\nString reversed thru brute force method is : ");
     System.out.println(reverseStringBruteForce(stringToBeReversed));
     endTime = System.nanoTime();
-    long bruteForceDuration = (endTime - startTime);
-    System.out.println("Brute force duration: "+bruteForceDuration+"ns");
+    
+    float bruteForceDuration = (endTime - startTime)/1000000;
+    System.out.println("Brute force duration: ");
+    System.out.printf("%.10f\n", bruteForceDuration);
 
+    
+    //Reversing String thru Recursion approach
     startTime=0;
     endTime=0;
+    
     startTime = System.nanoTime();
-    System.out.println("\nString reverse thru recursion method is : ");
+    System.out.println("String reversed thru recursion method is : ");
     System.out.println(reverseStringBruteForce(stringToBeReversed));
     endTime = System.nanoTime();
-    long recursionDuration = (endTime - startTime);
-    System.out.println("Recursion duration: "+recursionDuration+"ns");
     
+    float recursionDuration = (endTime - startTime)/1000000;
+    System.out.println("Recursion duration: ");
+    System.out.printf("%.10f\n", recursionDuration);
     scanner.close();
   }
 
   public static String reverseStringBruteForce(String stringToBeReversed){
     String stringReversed= "";
+    
     int stringLength = stringToBeReversed.length();
     for (int i = stringLength-1; i >= 0; i--) {
  
             System.out.print(stringReversed + stringToBeReversed.charAt(i));
     }
+    
     return stringReversed;
   }
   public static String reverseStringRecursion(String stringToBeReversed){
-    String stringReversed= "";
-    return stringReversed;
+    //base condition
+    if(stringToBeReversed==""){
+      return stringToBeReversed;
+    }else{
+      return reverseStringRecursion(stringToBeReversed.substring(1))+stringToBeReversed.charAt(0);
+    }
+    
   }
 }
