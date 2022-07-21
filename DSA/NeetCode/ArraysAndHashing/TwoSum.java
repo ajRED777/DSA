f6a6b01fda5c75b8c232f54a783cdb1e507d91c6
Alan J <44117428+ajRED777@users.noreply.github.com>
2022-07-21 03:47:32 +0000

    LeetCode 1:Two sum-skeleton

---
+++ DSA/NeetCode/ArraysAndHashing/TwoSum.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+class TwoSum {
+  public static void main(String[] args) {
+    System.out.println("Checking if array has integrs with target sum");
+    int[] nums = new int[] { 1, 1, 3, 1 };
+    int targetSum = 5;
+    checkTwoSumUsingHash(nums, targetSum);
+
+  }
+
+  public static int[] checkTwoSumUsingHash(int[] nums,int targetSum) {
+    // Checking 2 sum using hash table
+    System.out.println("Checking 2 sum using hash set...");
+    // logic starts...
+    int[] twoSumIndexes;
+
+    return twoSumIndexes;
+    // ...logic ends
+
+  }
+}
