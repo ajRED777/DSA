9320df52c597e85686ef30dbd71e76f7c88a3660
Alan J <44117428+ajRED777@users.noreply.github.com>
2022-07-26 07:10:08 +0000

    LeetCode 11:Container With Most Water-brute force

--- DSA/NeetCode/TwoPointers/ContainerWithMostWater.java
+++ DSA/NeetCode/TwoPointers/ContainerWithMostWater.java
@@ -16,28 +16,43 @@ import java.util.*;
 class ContainerWithMostWater {
   public static void main(String[] args) {
     System.out.println("Getting indices in array that form the largest water container");
-    int[] nums = new int[] {11,4,5,3,1,3,8};
-    System.out.println();
-    System.out.println(Arrays.toString(containerWithMostWaterBruteForce(nums)));
+    int[] nums = new int[] { 11, 4, 5, 31, 1, 3, 8 };
+    System.out.println("Values that form largest container");
+    int[] sides = containerWithMostWaterBruteForce(nums);
+    System.out.println(sides[0] + "," + sides[1]);
+    System.out.println("Container Area");
+    System.out.println(sides[2]);
 
   }
 
-  
   public static int[] containerWithMostWaterBruteForce(int[] nums) {
     // Checking 2 sum using brute force
     System.out.println("Getting Container With Most Water using brute force...");
     // logic starts...
 
     int[] sides = null;
-    largestArea = 0;
+    int largestArea = 0;
 
-    //for(i->nums.len-1)
-    //side1 = nums[i]
-    //for(i+1->nums.len-1)
-    //side2=nums[j]
-    //check if max(side1,side2)*(j-1) is largest area
-    
-    return null;
+    // for(i->nums.len-1)
+    // side1 = nums[i]
+    // for(i+1->nums.len-1)
+    // side2=nums[j]
+    // check if min(side1,side2)*(j-1) is largest area
+
+    for (int i = 0; i < nums.length; i++) {
+      int side1 = nums[i];
+      for (int j = i + 1; j < nums.length; j++) {
+        int side2 = nums[j];
+        int height = Math.min(side1, side2);
+        int currentArea = height * (j - i);
+        if (currentArea > largestArea) {
+          largestArea = currentArea;
+          sides = new int[] { nums[i], nums[j], largestArea };
+        }
+      }
+    }
+
+    return sides;
     // ...logic ends
 
   }
